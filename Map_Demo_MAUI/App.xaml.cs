ea552fb48dc3b6db398e06901550a3f6ce146d4f
Nirav Daraniya <nmdaraniya@gmail.com>
2023-01-06 23:41:06 +0530

    initial commit

---
+++ Map_Demo_MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Map_Demo_MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Map_Demo_MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Map_Demo_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
